36e5eb1208a02aaa8880e1ca251787258b984736
David Airlambang <david.airlambang@gmail.com>
2023-09-13 14:57:21 +0700

    action button and pagination

---
+++ client/src/app/gudang/columns.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Barang } from "../tabletype";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenuItem,
+  DropdownMenuLabel,
+} from "@radix-ui/react-dropdown-menu";
+
+export const columns: ColumnDef<Barang>[] = [
+  {
+    header: "Nama Barang",
+    accessorKey: "namaBarang",
+  },
+  {
+    header: "Jumlah Barang",
+    accessorKey: "jumlahBarang",
+  },
+  {
+    header: "Lokasi Gudang",
+    accessorKey: "lokasiGudang",
+  },
+  {
+    header: "Keterangan",
+    accessorKey: "keterangan",
+  },
+  {
+    header: "Last Update",
+    accessorKey: "updatedAt",
+    // modif
+    cell: ({ row }) => {
+      const updatedAt = row.getValue("updatedAt");
+      const formatted = new Date(updatedAt as string).toLocaleString();
+      return <div className="font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const barang = row.original;
+      const noBarang = barang.noBarang;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="w-8 h-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel className="font-bold pb-3">
+              Actions
+            </DropdownMenuLabel>
+            {/* ganti jdi action yang diinginkan */}
+            <DropdownMenuItem
+              className="pb-2 hover:backdrop-blur-sm"
+              onClick={() => {
+                navigator.clipboard.writeText(noBarang.toString());
+              }}
+            >
+              copy barang
+            </DropdownMenuItem>
+            {/* sampe sini */}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- client/src/app/gudang/data-table.tsx
+++ client/src/app/gudang/data-table.tsx
@@ -4,6 +4,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -15,6 +16,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -29,48 +31,78 @@ export function GudangDataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => {
-            return (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableHeader>
+    <div className="mt-10">
+      {/* table */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => {
+              return (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableHeader>
 
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell>No Results</TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell>No Results</TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      {/* pagination */}
+      <div className="flex items-center justify-start space-x-2 py-4">
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          onClick={() => {
+            table.previousPage();
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          onClick={() => {
+            table.nextPage();
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
