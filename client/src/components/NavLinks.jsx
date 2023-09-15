7d9492aab38a3440d9134c1d17137468c6cc3016
David Airlambang <david.airlambang@gmail.com>
2023-09-15 20:39:36 +0700

    update view navlinks

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,24 +1,26 @@
-import { useDashboardContext } from '../pages/DashboardLayout';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
+import { useDashboardContext } from "../pages/DashboardLayout";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
 
+// eslint-disable-next-line react/prop-types
 const NavLinks = ({ isBigSidebar }) => {
   const { toggleSidebar, user } = useDashboardContext();
   return (
-    <div className='nav-links'>
+    <div className="nav-links">
       {links.map((link) => {
         const { text, path, icon } = link;
         const { role } = user;
-        if (path === 'admin' && role !== 'admin') return;
+        const adminOnly = ["gudang"];
+        if (adminOnly.includes(path) && role !== "admin") return;
         return (
           <NavLink
             to={path}
             key={text}
-            className='nav-link'
+            className="nav-link"
             onClick={isBigSidebar ? null : toggleSidebar}
             end
           >
-            <span className='icon'>{icon}</span>
+            <span className="icon">{icon}</span>
             {text}
           </NavLink>
         );
