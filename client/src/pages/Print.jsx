46e0990567ef6f2bb886dad3629be121d91a5978
David Airlambang <david.airlambang@gmail.com>
2023-11-28 21:33:28 +0700

    print laporan

--- client/src/pages/Print.jsx
+++ client/src/pages/Print.jsx
@@ -1,7 +1,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableFooter,
   TableHead,
@@ -9,11 +8,26 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { format } from "date-fns";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 export function Print() {
+  const [isPrinted, setIsPrinted] = useState(false);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+
+  const handlePrintClick = () => {
+    setIsPrinted(true);
+
+    setTimeout(() => {
+      window.print();
+    }, 150);
+
+    setTimeout(() => {
+      setIsPrinted(false);
+    }, 250);
+  };
 
   const filterKomisi = queryParams.get("filterKomisi");
   const penerimaanData = JSON.parse(
@@ -28,11 +42,24 @@ export function Print() {
 
   // Pastikan data ada sebelum digunakan
   const penerimaans = penerimaanData || [];
+  const pengeluarans = pengeluaranData || [];
   return (
-    <body className="text-black bg-white">
+    <body className={`${isPrinted ? "text-black" : ""} bg-white max-w-4xl`}>
       <div className="max-w-full p-4">
         <h2 className="pb-4 text-center">{`Laporan Keuangan ${filterKomisi}`}</h2>
-        <h4 className="py-4">Penerimaan</h4>
+
+        {/* print */}
+        <Button
+          onClick={handlePrintClick}
+          className={`flex justify-between p-3  ${
+            isPrinted ? "invisible" : ""
+          }`}
+        >
+          Print
+        </Button>
+
+        {/* Penerimaan */}
+        <h3 className="pt-4">Penerimaan</h3>
         <Table className="table-auto w-full max-w-[100%] md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] 2xl:max-w-[50%] mx-auto">
           <TableHeader>
             <TableRow>
@@ -70,10 +97,15 @@ export function Print() {
           </TableBody>
           <TableFooter>
             <TableRow>
-              <TableCell colSpan={2} className="font-medium text-black">
+              <TableCell
+                colSpan={2}
+                className={`font-medium ${isPrinted ? "text-black" : ""}`}
+              >
                 Total
               </TableCell>
-              <TableCell className="text-right text-black">
+              <TableCell
+                className={`text-right ${isPrinted ? "text-black" : ""}`}
+              >
                 {new Intl.NumberFormat("id-ID", {
                   style: "currency",
                   currency: "IDR",
@@ -83,7 +115,115 @@ export function Print() {
             </TableRow>
           </TableFooter>
         </Table>
+
+        {/* pengeluaran */}
+        <h3 className="py-4 mt-4">Pengeluaran</h3>
+        <Table className="table-auto w-full max-w-[100%] md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] 2xl:max-w-[50%] mx-auto">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Tanggal</TableHead>
+              <TableHead>Nama Program</TableHead>
+              <TableHead>Nominal</TableHead>
+              <TableHead className="text-right">Penerima</TableHead>
+              <TableHead className="text-right">Uraian</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {pengeluarans.map((pengeluaran) => (
+              <TableRow key={pengeluaran.noAdministrasi}>
+                <TableCell className="font-medium">
+                  {format(
+                    new Date(pengeluaran.tanggalAdministrasi),
+                    "dd-MM-yyyy"
+                  )}
+                </TableCell>
+                <TableCell>{pengeluaran.namaProgram}</TableCell>
+                <TableCell>
+                  {new Intl.NumberFormat("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(pengeluaran.nominalAdministrasi)}
+                </TableCell>
+                <TableCell className="text-right">
+                  {pengeluaran.penerimaAdministrasi}
+                </TableCell>
+                <TableCell className="text-right">
+                  {pengeluaran.uraianAdministrasi}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={2} className="font-medium text-black">
+                Total
+              </TableCell>
+              <TableCell className="text-right text-black">
+                {new Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                }).format(totalPengeluaran)}
+              </TableCell>
+              <TableCell colSpan={2} className="font-medium"></TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+
+        <div>
+          <br />
+          <p className="py-2 pb-1">{`Saldo Periode yang dipilih : ${new Intl.NumberFormat(
+            "id-ID",
+            {
+              style: "currency",
+              currency: "IDR",
+            }
+          ).format(totalPenerimaan - totalPengeluaran)}`}</p>
+          <p className="py-1">{`Saldo Awal : ${new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+          }).format(saldoAwal)}`}</p>
+          <p className="py-1">{`Saldo Akhir : ${new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+          }).format(saldoAwal + totalPenerimaan - totalPengeluaran)}`}</p>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
+      <footer
+        className={`flex justify-between p-3  ${isPrinted ? "" : "invisible"}`}
+      >
+        {/* Tandatangan dan nama untuk disetujui (di kiri) */}
+        <div className="text-center">
+          <p>Disetujui Oleh</p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>
+            (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+          </p>
+        </div>
+
+        {/* Tandatangan dan nama untuk dibuat (di kanan) */}
+        <div className="text-center">
+          <p>Dibuat Oleh</p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>
+            (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+          </p>
+        </div>
+      </footer>
     </body>
   );
 }
