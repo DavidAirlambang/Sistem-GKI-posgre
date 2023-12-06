625e0aae98c201d3d06a8bd28f615abecf9d1f3f
David Airlambang <david.airlambang@gmail.com>
2023-12-06 10:50:11 +0700

    ready to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
+        // #target tergantung di deploy dimana
         target: 'http://localhost:5000/api',
         // target: 'http://34.101.188.146:5000/api'
         // target: 'http://api-proker.gkitamancibunut.org/api',
